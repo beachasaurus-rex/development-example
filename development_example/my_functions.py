8a20c07fc3777ccc864108accfc654afd30a6cc9
jcrizk <jonathan.rizk@rockwellautomation.com>
2024-08-08 08:59:29 -0500

    simplifies example

--- development_example/my_functions.py
+++ development_example/my_functions.py
@@ -1,12 +1,8 @@
-# This function returns the relative difference between `reference` and `num_to_compare`.
-def rel_diff(num_to_compare, reference):
-    if reference == 0:
-        return num_to_compare - reference
-    else:
-        return (num_to_compare - reference) / reference
-
-
 # This function returns True if `num_to_compare` is near `reference`, within the specified tolerance `tol`.
 def is_near_number(num_to_compare, reference, tol):
-    err = abs(rel_diff(num_to_compare, reference))
-    return err <= tol
+    rel_diff = 0
+    if reference == 0:
+        rel_diff = abs(num_to_compare - reference)
+    else:
+        rel_diff = abs((num_to_compare - reference) / reference)
+    return rel_diff <= tol
